5c8ae6bd4d558ad90719f4b05236c260eb30afcd
songrim <srimm3399@naver.com>
2024-02-06 15:10:10 +0900

    search form add

--- form/login.php
+++ form/login.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
 </head>
 <body>
-  
+    <h1>login</h1>
+    <form action="request.php" method="post">
+        <p>
+            <label for="name">Name: </label>
+            <input type="text" name="username" id="name">    
+        </p>
+        <p>
+            <label for="email">Email: </label>
+            <input type="email" name="useremail" id="email">    
+        </p>
+        <p>
+            <input type="submit" value="로그인">
+        </p>
+    </form>
+    <hr>
+    <form action="search.php">
+      <input type="search" name="keyword">
+      <button>검색</button>
+    </form>
 </body>
 </html>
---
+++ form/request.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Request</title>
+</head>
+<body>
+  <h1>회원가입 완료</h1>
+  <!-- 
+    홍길동님 hong@abc.com주소로 회원가입을 완료했습니다.
+   -->
+  <?php
+  // print_r($_POST)
+  echo "{$_POST['username']}님 {$_POST['useremail']}주소로 회원가입을 완료했습니다.";
+
+  ?>
+</body>
+</html>
